e9f38dc7e0fa436147aea8cf5eb6de6c2eb1dbb4
Darrell2000 <ddavis811@ivytech.edu>
2024-09-01 15:20:40 -0400

    Add files via upload

---
+++ Pseudocode Module 2 - Exercise 2 Darrell Davis.py
@@ -0,0 +1,13 @@
+main
+start
+  accountBalance = input
+   overDrawnCount = input
+    overDrawnFee = (accountBalance * 0.01) - (overDrawnCount * 5)
+newAccountBalance = accountBalance - overDrawnFee
+Output
+print = overDrawnFee
+Output
+print= newAccountBalance
+Output
+print =Thanks using this program
+main
